9d644c09491c2835d90c121aa85fc8e67e6fa961
iyoungman <hsl9546@gmail.com>
2019-05-19 16:04:11 +0900

    Defalut Package 구조

---
+++ src/main/java/com/youngman/sample/model/Sample.java
@@ -0,0 +1,8 @@
+package com.youngman.sample.model;
+
+/**
+ * Created by YoungMan on 2019-05-19.
+ */
+
+public class Sample {
+}
---
+++ src/main/java/com/youngman/sample/repository/Sample.java
@@ -0,0 +1,8 @@
+package com.youngman.sample.repository;
+
+/**
+ * Created by YoungMan on 2019-05-19.
+ */
+
+public class Sample {
+}
